3a32eff6c2a16938e87ac6ea46e3c13a8f207856
Trevor Pierce <tpierce@boydcatongroup.com>
2017-06-30 15:45:56 -0500

    Adding a fix that removes the arrow event listener, and relies on tabbing to switch tabs. Also focuses the <section> when tab link is clicked.

--- client.js
+++ client.js
@@ -8,42 +8,33 @@ document.addEventListener('DOMContentLoaded', function () {
 			cache[tab.hash] = [tab, document.getElementById(tab.getAttribute('aria-controls'))];
 
 			if (tab.getAttribute('aria-selected') === 'true') {
-				last = cache[''] = cache[tab.hash];
+				last = cache[tab.hash];
 			} else {
-				tab.setAttribute('tabindex', -1);
+				tab.setAttribute('tabindex', 0);
 			}
-
-			tab.addEventListener('keydown', function (event) {
-				var next = event.keyCode === 37 ? tabs[index - 1] : event.keyCode === 39 ? tabs[index + 1] : null;
-
-				if (next) {
-					location.hash = next.hash;
-
-					next.focus();
-				}
-			});
 		});
 	});
-
-	window.addEventListener('hashchange', onhashchange);
-
-	onhashchange();
-
+	
 	function onhashchange() {
 		var tab = cache[location.hash];
 
 		if (tab) {
 			if (last) {
 				last[0].removeAttribute('aria-selected');
-				last[0].setAttribute('tabindex', -1);
+				last[0].setAttribute('tabindex', 0);
 				last[1].setAttribute('hidden', '');
 			}
 
 			tab[0].setAttribute('aria-selected', 'true');
-			tab[0].removeAttribute('tabindex');
 			tab[1].removeAttribute('hidden', '');
+			tab[1].setAttribute('tabindex', -1);
+			tab[1].focus();
 
 			last = tab;
 		}
 	}
+
+	window.addEventListener('hashchange', onhashchange);
+
+	onhashchange();
 });
